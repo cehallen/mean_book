a379c4aa61d3c8f695aa6fdd52e40025b2a4e7bf
Chris Allen <cehallen@gmail.com>
2015-06-23 15:39:22 -0400

    Define custom model method 'findOneByUsername'

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,10 +4,14 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: String,
+    email: {
+        type: String,
+        index: true
+    }, 
     username: {
         type: String,
-        trim: true
+        trim: true,
+        unique: true
     },
     password: String,
     created: {
@@ -29,8 +33,16 @@ var UserSchema = new Schema({
     }
 });
 
+UserSchema.statics.findOneByUsername = function (username, callback) {
+    this.findOne({ username: new RegExp(username, 'i') }, callback);    
+};
+
 UserSchema.virtual('fullName').get(function() {
     return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+    var splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
 });
 
 UserSchema.set('toJSON', { getters: true, virtuals: true });
